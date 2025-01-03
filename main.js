68b81869860bc07705b421e76b12624992d4b60e
gorilla <oatmeal7640@gmail.com>
2025-01-04 01:29:06 +0700

    added main gameplay variables

--- main.js
+++ main.js
@@ -1,13 +1,33 @@
+// NOTE: learn abt objects first
+//       dumbfuck
+
+
 // main variables for game's whole function
 let valueMain = 0;
+let valuePower = 1;
 let startBtn = document.getElementById("start");
+let incrementBtn = document.getElementById("increment");
+let decrementBtn = document.getElementById("decrement");
+let resetBtn = document.getElementById("reset");
+let gameState = false;
 
-// you don't need a comment for this.
+// simple update function to make code more readable and simple 
 function update() {
   document.getElementById("value").innerHTML = valueMain;
+  gameState = true
 }
 
 // start button to give sort of a main menu type vibe
 startBtn.addEventListener("click", function () {
   update();
 });
+
+// main game functions
+if (gameState) {
+  incrementBtn.addEventListener("click", function () {
+  valueMain += valuePower;
+  update();
+});  
+  
+  
+}
